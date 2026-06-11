38b0356ed1e79ffb37aebef5997040c4b1af8d91
sumit212-dev <132038818+sumit2221-div@users.noreply.github.com>
2026-06-11 08:48:28 +0530

    installed react

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,13 @@
 import express from 'express';
 
 const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
